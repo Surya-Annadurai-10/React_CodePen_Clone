54e3fcab32dbe1fd6105a1a32c4180f0679cab98
Surya Annadurai <suryaannadurai1041999@gmail.com>
2025-02-22 16:51:09 +0530

    editor section setted succesfully

--- src/Containers/PenEditor.jsx
+++ src/Containers/PenEditor.jsx
@@ -1,14 +1,39 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
 import { addUserData, loggedIn } from "../slices/slice";
 import SplitPane, { Pane } from "react-split-pane";
+import { FaChevronDown, FaHtml5 } from "react-icons/fa";
+import { FaCss3 } from "react-icons/fa";
+import { FaJsSquare } from "react-icons/fa";
+import { IoMdSettings } from "react-icons/io";
+import CodeMirror from "@uiw/react-codemirror";
+import {javascript} from "@codemirror/lang-javascript"
+
 
 const PenEditor = () => {
   const { id } = useParams();
   // const location  = useLocation();
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const [html, setHtml] = useState("")
+  const [css, setCss] = useState("")
+  const [js, setJs] = useState("")
+  const [result, setResult] = useState("")
+
+
+  useEffect(() =>{
+    
+    let output = `
+    <html>
+    <style>${css}</style>
+    <body>${html}</body>
+    <script>${js}</script>
+    </html>`
+
+    setResult(output)
+
+  },[html , js, css])
 
   useEffect(() => {
     let user = JSON.parse(localStorage.getItem("userData"));
@@ -25,42 +50,106 @@ const PenEditor = () => {
 
   return (
     <>
-   <header className="w-full h-[10vh] bg-[#010101]">
-    <div> 
-      <div>
-        <img src="" alt="" />
+     
+     <div className="w-full h-[90vh] relative">
+     <div className="w-full h-[80%] bg-[#0f0f0f] overflow-hidden">
+        <div className=" w-full h-[full]]  overflow-hidden">
+          <SplitPane split="horizontal" maxSize={"50%"}  minSize={"50%"} defaultSize={"60%"}>
+            <div>
+            <SplitPane split="vertical" defaultSize={"70%"} minSize={500}>
+              <SplitPane defaultSize={"50%"}>
+                <div>
+                  <header className="text-[#9DAEBC]">
+                    <div className="flex justify-between items-center">
+                      <div className="flex items-center gap-1 w-[fit-content] bg-[#1D1E22] border-t-3 border-t-[#34363E] p-2">
+                        <FaHtml5 className="text-2xl text-red-500" />
+                        <h3>HTML</h3>
+                      </div>
+                      <div className="flex bg-[#1D1E22] gap-2 items-center border-t-[#34363E] border-t-3  py-3 p-2">
+                        <IoMdSettings />
+                        <FaChevronDown />
+                      </div>
+                    </div>
+                  </header>
+                  <section className="w-full px-2">
+                 
+                      <CodeMirror
+                        value={html}
+                       height="350px"
+                        className="w-full h-full"
+                        extensions={[javascript({ jsx: true })]}
+                        theme={"dark"}
+                        onChange={(value , viewUpdate) => setHtml(value)}
+                      />
+                    
+                  </section>
+                </div>
+                <div>
+                  <header className="text-[#9DAEBC]">
+                    <div className="flex justify-between items-center">
+                      <div className="flex items-center gap-1 w-[fit-content] bg-[#1D1E22] border-t-3 border-t-[#34363E] p-2">
+                        <FaCss3 className="text-sky-500 text-2xl" />
+                        <h3>CSS</h3>
+                      </div>
+                      <div className="flex bg-[#1D1E22] gap-2 items-center border-t-[#34363E] border-t-3  py-3 p-2">
+                        <IoMdSettings />
+                        <FaChevronDown />
+                      </div>
+                    </div>
+                  </header>
+                  <section className="w-full px-2">
+                 
+                      <CodeMirror
+                        value={css}
+                       height="350px"
+                        className="w-full h-full"
+                        extensions={[javascript({ jsx: true })]}
+                        theme={"dark"}
+                        onChange={(value , viewUpdate) => setCss(value)}
+                      />
+                    
+                  </section>
+                </div>
+              </SplitPane>
+              <div>
+                <header className="text-[#9DAEBC]">
+                  <div className="flex justify-between items-center">
+                    <div className="flex items-center gap-1 w-[fit-content] bg-[#1D1E22] border-t-3 border-t-[#34363E] p-2">
+                      <FaJsSquare className="text-yellow-300 text-2xl" />
+                      <h3>JS</h3>
+                    </div>
+                    <div className="flex bg-[#1D1E22] gap-2 items-center border-t-[#34363E] border-t-3  py-3 p-2">
+                      <IoMdSettings />
+                      <FaChevronDown />
+                    </div>
+                  </div>
+                </header>
+                <section className="w-full px-2">
+                 
+                      <CodeMirror
+                        value={js}
+                       height="350px"
+                        className="w-full h-full"
+                        extensions={[javascript({ jsx: true })]}
+                        theme={"dark"}
+                        onChange={(value , viewUpdate) => setJs(value)}
+                      />
+                    
+                  </section>
+              </div>
+            </SplitPane>
+            </div>
+            {/* wepage */}
+             
+          
+              <div className="bg-[white] overflow-hidden h-full w-full">
+                <iframe srcDoc={result} title="result" style={{border:"none" , width:"100%" , height:"100%"}}  frameborder="0"></iframe>
+              </div>
+            
+          </SplitPane>
+        </div>
       </div>
-      <div>
-
-      </div>
-    </div>
-    <div></div>
-   </header>
-<div className="w-full h-[90vh] bg-[#0f0f0f] overflow-hidden">
-<div className=" w-full h-[full]]  overflow-hidden">
-        <SplitPane 
-        split="horizontal"
-        minSize={"50%"}
-        defaultSize={"50%"}
-        >
-        <SplitPane  split="vertical" defaultSize={"70%"} minSize={500}>
-           <SplitPane defaultSize={"50%"}>
-           <div>
-            <h1 className="text-white">html</h1>
-           </div>
-           <div>
-            <h1 className="text-white">css</h1>
-           </div>
-           </SplitPane>
-           <div>
-            <h1 className="text-white">js</h1>
-           </div>
-        </SplitPane>
-           
-        </SplitPane>
-      </div>
-</div>
-
+     </div>
     </>
   );
 };

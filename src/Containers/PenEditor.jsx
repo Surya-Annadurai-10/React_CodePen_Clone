821c83ec46eeb969725a6bc67cc14eafdea5e4fb
Surya Annadurai <suryaannadurai1041999@gmail.com>
2025-02-22 08:55:21 +0530

    Code Editor Layout has completed

--- src/Containers/PenEditor.jsx
+++ src/Containers/PenEditor.jsx
@@ -1,34 +1,68 @@
-import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux';
-import { Link, useLocation, useNavigate, useParams } from 'react-router-dom'
-import { addUserData, loggedIn } from '../slices/slice';
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
+import { addUserData, loggedIn } from "../slices/slice";
+import SplitPane, { Pane } from "react-split-pane";
 
 const PenEditor = () => {
-    const {id} = useParams();
-    // const location  = useLocation();
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
+  const { id } = useParams();
+  // const location  = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    useEffect(() =>{
-         let user = JSON.parse(localStorage.getItem("userData"))
-         if(user){
-           dispatch(loggedIn(true));
-           dispatch(addUserData(user))
-           
-         }else  {
-           dispatch(loggedIn(false))
-           navigate("/home/trending")
-         };
-       },[])
-    console.log("params", id);
-    console.log("location", location);
-    
+  useEffect(() => {
+    let user = JSON.parse(localStorage.getItem("userData"));
+    if (user) {
+      dispatch(loggedIn(true));
+      dispatch(addUserData(user));
+    } else {
+      dispatch(loggedIn(false));
+      navigate("/home/trending");
+    }
+  }, []);
+  console.log("params", id);
+  console.log("location", location);
+
   return (
-    <div>
-        <h1>PenEditor</h1>
-       <Link className={"bg-red-300"} to={"/home/trending"} >Back</Link>
-    </div>
-  )
-}
+    <>
+   <header className="w-full h-[10vh] bg-[#010101]">
+    <div> 
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div>
 
-export default PenEditor
+      </div>
+    </div>
+    <div></div>
+   </header>
+<div className="w-full h-[90vh] bg-[#0f0f0f] overflow-hidden">
+<div className=" w-full h-[full]]  overflow-hidden">
+        <SplitPane 
+        split="horizontal"
+        minSize={"50%"}
+        defaultSize={"50%"}
+        >
+        <SplitPane  split="vertical" defaultSize={"70%"} minSize={500}>
+           <SplitPane defaultSize={"50%"}>
+           <div>
+            <h1 className="text-white">html</h1>
+           </div>
+           <div>
+            <h1 className="text-white">css</h1>
+           </div>
+           </SplitPane>
+           <div>
+            <h1 className="text-white">js</h1>
+           </div>
+        </SplitPane>
+           
+        </SplitPane>
+      </div>
+</div>
+
+    </>
+  );
+};
+
+export default PenEditor;
